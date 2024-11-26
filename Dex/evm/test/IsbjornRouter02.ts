5a3565d71de529524ad031bbf692c1f9b93d7a60
Mach1Btc <allxcaps@gmail.com>
2024-11-26 00:01:50 -0600

    remove some useless comments

--- Dex/evm/test/IsbjornRouter02.ts
+++ Dex/evm/test/IsbjornRouter02.ts
@@ -32,22 +32,18 @@ describe("IsbjornRouter02", function () {
     [owner, user1, user2, user3] = accounts;
     ownerAddress = await owner.getAddress();
 
-    // Deploy factory
     const Factory = await ethers.getContractFactory("IcePondFactory");
     factory = await Factory.deploy(ownerAddress);
     await factory.waitForDeployment();
 
-    // Deploy WAVAX
     const Wavax = await ethers.getContractFactory("WAVAXMock");
     WAVAX = await Wavax.deploy();
     await WAVAX.waitForDeployment();
 
-    // Deploy router
     const Router = await ethers.getContractFactory("IsbjornRouter02");
     router = await Router.deploy(factory.target, WAVAX.target);
     await router.waitForDeployment();
 
-    // Deploy mock tokens
     const Token = await ethers.getContractFactory("ERC20Mock");
     tokenA = await Token.deploy(
       "TokenA",
@@ -82,15 +78,12 @@ describe("IsbjornRouter02", function () {
 
         const deadline = latestBlock.timestamp + 3600;
 
-        // Record initial balances
         const initialOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
         const initialOwnerTokenBBalance = await tokenB.balanceOf(ownerAddress);
 
-        // Approve router to transfer tokens
         await tokenA.connect(owner).approve(router.target, amountADesired);
         await tokenB.connect(owner).approve(router.target, amountBDesired);
 
-        // Call addLiquidity with isVolatile = true
         const tx: ContractTransactionResponse = await router.addLiquidity(
           true, // isVolatile
           tokenA.target,
@@ -110,7 +103,6 @@ describe("IsbjornRouter02", function () {
           );
         }
 
-        // Record new balances after liquidity addition
         const newOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
         const newOwnerTokenBBalance = await tokenB.balanceOf(ownerAddress);
 
@@ -120,11 +112,9 @@ describe("IsbjornRouter02", function () {
           tokenB.target
         );
 
-        // Record new liquidity pool pair balances
         const newPairTokenABalance = await tokenA.balanceOf(pairAddress);
         const newPairTokenBBalance = await tokenB.balanceOf(pairAddress);
 
-        // Assertions
         expect(newOwnerTokenABalance).to.be.lessThanOrEqual(
           initialOwnerTokenABalance - ethers.parseEther("9")
         );
@@ -151,16 +141,13 @@ describe("IsbjornRouter02", function () {
 
         const deadline = latestBlock.timestamp + 3600;
 
-        // Record initial balances
         const initialOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
         const initialOwnerTokenBBalance = await ethers.provider.getBalance(
           ownerAddress
         );
 
-        // Approve router to transfer tokens
         await tokenA.connect(owner).approve(router.target, amountADesired);
 
-        // Call addLiquidity with isVolatile = true
         const tx: ContractTransactionResponse = await router.addLiquidityAVAX(
           true, // isVolatile
           tokenA.target,
@@ -179,7 +166,6 @@ describe("IsbjornRouter02", function () {
           );
         }
 
-        // Record new balances after liquidity addition
         const newOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
         const newOwnerTokenBBalance = await ethers.provider.getBalance(
           ownerAddress
@@ -191,11 +177,9 @@ describe("IsbjornRouter02", function () {
           WAVAX.target
         );
 
-        // Record new liquidity pool pair balances
         const newPairTokenABalance = await tokenA.balanceOf(pairAddress);
         const newPairTokenBBalance = await WAVAX.balanceOf(pairAddress);
 
-        // Assertions
         expect(newOwnerTokenABalance).to.be.lessThanOrEqual(
           initialOwnerTokenABalance - ethers.parseEther("9")
         );
@@ -222,17 +206,14 @@ describe("IsbjornRouter02", function () {
 
         const deadline = latestBlock.timestamp + 3600;
 
-        // Record initial balances
         const initialOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
 
-        // Approve router to transfer tokens
         await tokenA.connect(owner).approve(router.target, amountADesired);
         await WAVAX.connect(owner).deposit({ value: amountBDesired });
         await WAVAX.connect(owner).approve(router.target, amountBDesired);
 
         const initialOwnerTokenBBalance = await WAVAX.balanceOf(ownerAddress);
 
-        // Call addLiquidity with isVolatile = true
         const tx: ContractTransactionResponse = await router.addLiquidity(
           true, // isVolatile
           tokenA.target,
@@ -252,7 +233,6 @@ describe("IsbjornRouter02", function () {
           );
         }
 
-        // Record new balances after liquidity addition
         const newOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
         const newOwnerTokenBBalance = await WAVAX.balanceOf(ownerAddress);
 
@@ -262,11 +242,9 @@ describe("IsbjornRouter02", function () {
           WAVAX.target
         );
 
-        // Record new liquidity pool pair balances
         const newPairTokenABalance = await tokenA.balanceOf(pairAddress);
         const newPairTokenBBalance = await WAVAX.balanceOf(pairAddress);
 
-        // Assertions
         expect(newOwnerTokenABalance).to.be.lessThanOrEqual(
           initialOwnerTokenABalance - ethers.parseEther("9")
         );
@@ -295,11 +273,9 @@ describe("IsbjornRouter02", function () {
 
         const deadline = latestBlock.timestamp + 3600;
 
-        // Approve router to transfer tokens
         await tokenA.connect(owner).approve(router.target, amountADesired);
         await tokenB.connect(owner).approve(router.target, amountBDesired);
 
-        // Call addLiquidity with isVolatile = true
         const tx1: ContractTransactionResponse = await router.addLiquidity(
           true, // isVolatile
           tokenA.target,
@@ -314,7 +290,6 @@ describe("IsbjornRouter02", function () {
 
         await tokenA.connect(owner).approve(router.target, amountADesired);
 
-        // Call addLiquidity with isVolatile = true
         const tx2: ContractTransactionResponse = await router.addLiquidityAVAX(
           true, // isVolatile
           tokenA.target,
@@ -442,106 +417,4 @@ describe("IsbjornRouter02", function () {
       });
     });
   });
-
-  //   it("should remove liquidity successfully when isVolatile = true", async function () {
-  //     const [owner] = accounts;
-  //     const liquidity = ethers.parseEther("5");
-  //     const amountAMin = ethers.parseEther("4.5");
-  //     const amountBMin = ethers.parseEther("9");
-  //     const latestBlock = await ethers.provider.getBlock("latest");
-  //     if (!latestBlock) {
-  //       throw new Error("Failed to fetch the latest block.");
-  //     }
-  //     const deadline = latestBlock.timestamp + 3600;
-
-  //     // Mock liquidity token for the pair
-  //     const pairAddress = await factory.getPair(
-  //       true,
-  //       tokenA.target,
-  //       tokenB.target
-  //     );
-  //     const PairToken = await ethers.getContractFactory("IcePond");
-  //     const liquidityToken = PairToken.attach(pairAddress) as IcePond;
-
-  //     // Approve router to transfer liquidity token
-  //     await liquidityToken.connect(owner).approve(router.target, liquidity);
-
-  //     // Call removeLiquidity with isVolatile = true
-  //     const tx: ContractTransactionResponse = await router.removeLiquidity(
-  //       true, // isVolatile
-  //       tokenA.target,
-  //       tokenB.target,
-  //       liquidity,
-  //       amountAMin,
-  //       amountBMin,
-  //       ownerAddress,
-  //       deadline
-  //     );
-
-  //     const receipt = await tx.wait();
-  //     if (!receipt) {
-  //       throw new Error(
-  //         "Transaction receipt is null. The transaction may not have been mined."
-  //       );
-  //     }
-  //     const liquidityEvent = receipt.logs
-  //       .map((log) => {
-  //         try {
-  //           return router.interface.parseLog(log);
-  //         } catch (e) {
-  //           return null;
-  //         }
-  //       })
-  //       .find((parsed) => parsed?.name === "LiquidityRemoved");
-  //     expect(liquidityEvent).to.not.be.undefined;
-  //     const [amountA, amountB] = liquidityEvent!.args!;
-  //     expect(amountA).to.be.at.least(amountAMin);
-  //     expect(amountB).to.be.at.least(amountBMin);
-  //   });
-
-  //   it("should perform a swap successfully when isVolatile = true", async function () {
-  //     const [owner] = accounts;
-  //     const amountIn = ethers.parseEther("10");
-  //     const amountOutMin = ethers.parseEther("18");
-  //     const path = [tokenA.target, tokenB.target];
-  //     const latestBlock = await ethers.provider.getBlock("latest");
-  //     if (!latestBlock) {
-  //       throw new Error("Failed to fetch the latest block.");
-  //     }
-  //     const deadline = latestBlock.timestamp + 3600;
-
-  //     // Approve router to transfer tokens
-  //     await tokenA.connect(owner).approve(router.target, amountIn);
-
-  //     // Call swapExactTokensForTokens with isVolatile = true
-  //     const tx: ContractTransactionResponse =
-  //       await router.swapExactTokensForTokens(
-  //         true, // isVolatile
-  //         amountIn,
-  //         amountOutMin,
-  //         path,
-  //         ownerAddress,
-  //         deadline
-  //       );
-
-  //     const receipt = await tx.wait();
-  //     if (!receipt) {
-  //       throw new Error(
-  //         "Transaction receipt is null. The transaction may not have been mined."
-  //       );
-  //     }
-  //     const swapEvent = receipt.logs
-  //       .map((log) => {
-  //         try {
-  //           return router.interface.parseLog(log);
-  //         } catch (e) {
-  //           return null;
-  //         }
-  //       })
-  //       .find((parsed) => parsed?.name === "Swap");
-  //     expect(swapEvent).to.not.be.undefined;
-  //     const [amountInResult, amountOutResult] = swapEvent!.args!;
-  //     expect(amountInResult).to.equal(amountIn);
-  //     expect(amountOutResult).to.be.at.least(amountOutMin);
-  //   });
 });
