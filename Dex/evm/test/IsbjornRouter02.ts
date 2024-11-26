26e0731fa45673f338cdfec9ee91eac05043062f
Mach1Btc <allxcaps@gmail.com>
2024-11-26 13:07:29 -0600

    add few more tests

--- Dex/evm/test/IsbjornRouter02.ts
+++ Dex/evm/test/IsbjornRouter02.ts
@@ -20,21 +20,29 @@ describe("IsbjornRouter02", function () {
   let accounts: Signer[];
   let owner: Signer;
   let ownerAddress: string | Addressable;
+  let feeCollector: Signer;
+  let feeCollectorAddress: string | Addressable;
   let user1: Signer;
+  let user1Address: string | Addressable;
   let user2: Signer;
+  let user2Address: string | Addressable;
   let user3: Signer;
+  let user3Address: string | Addressable;
 
   let pairTokenFactory: IcePond__factory;
 
   beforeEach(async function () {
     accounts = await ethers.getSigners();
 
-    [owner, user1, user2, user3] = accounts;
+    [owner, feeCollector, user1, user2, user3] = accounts;
     ownerAddress = await owner.getAddress();
+    feeCollectorAddress = await feeCollector.getAddress();
 
     const Factory = await ethers.getContractFactory("IcePondFactory");
     factory = await Factory.deploy(ownerAddress);
     await factory.waitForDeployment();
+
+    await factory.setFeeTo(feeCollectorAddress);
 
     const Wavax = await ethers.getContractFactory("WAVAXMock");
     WAVAX = await Wavax.deploy();
@@ -64,7 +72,7 @@ describe("IsbjornRouter02", function () {
     pairTokenFactory = await ethers.getContractFactory("IcePond");
   });
   describe("Liquidity Functions", function () {
-    describe("Add Liquidity", function () {
+    describe("Add New Pair Liquidity", function () {
       it("should add liquidity for 2 tokens successfully", async function () {
         const amountADesired = ethers.parseEther("10");
         const amountBDesired = ethers.parseEther("20");
@@ -244,7 +252,7 @@ describe("IsbjornRouter02", function () {
         );
       });
     });
-    describe("Remove Liquidity", function () {
+    describe("Remove Recent New Pair Liquidity", function () {
       beforeEach(async function () {
         const amountADesired = ethers.parseEther("10");
         const amountBDesired = ethers.parseEther("20");
@@ -387,6 +395,201 @@ describe("IsbjornRouter02", function () {
         expect(newPairTokenBBalance).to.be.lessThanOrEqual(
           pairTokenBBalance - ethers.parseEther("18")
         );
+      });
+      it("should add liquidity for a token and WAVAX successfully", async function () {
+        const latestBlock = await ethers.provider.getBlock("latest");
+
+        if (!latestBlock) {
+          throw new Error("Failed to fetch the latest block.");
+        }
+
+        const deadline = latestBlock.timestamp + 3600;
+        const pairAddress = await router.pairFor(tokenA.target, WAVAX.target);
+
+        const initialOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
+        const initialOwnerTokenBBalance = await WAVAX.balanceOf(ownerAddress);
+
+        const pairTokenABalance = await tokenA.balanceOf(pairAddress);
+        const pairTokenBBalance = await WAVAX.balanceOf(pairAddress);
+
+        const pair: IcePond = pairTokenFactory.attach(pairAddress) as IcePond;
+        const ownerLiquidityBalance = await pair.balanceOf(ownerAddress);
+
+        await pair.connect(owner).approve(router.target, ownerLiquidityBalance);
+
+        const tx: ContractTransactionResponse = await router.removeLiquidity(
+          tokenA.target,
+          WAVAX.target,
+          ownerLiquidityBalance,
+          ethers.parseEther("9"),
+          ethers.parseEther("18"),
+          ownerAddress,
+          deadline
+        );
+
+        const newPairTokenABalance = await tokenA.balanceOf(pairAddress);
+        const newPairTokenBBalance = await WAVAX.balanceOf(pairAddress);
+
+        const newOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
+        const newOwnerTokenBBalance = await WAVAX.balanceOf(ownerAddress);
+
+        expect(newOwnerTokenABalance).to.be.greaterThanOrEqual(
+          initialOwnerTokenABalance + ethers.parseEther("9")
+        );
+        expect(newOwnerTokenBBalance).to.be.greaterThanOrEqual(
+          initialOwnerTokenBBalance + ethers.parseEther("18")
+        );
+        expect(newPairTokenABalance).to.be.lessThanOrEqual(
+          pairTokenABalance - ethers.parseEther("9")
+        );
+        expect(newPairTokenBBalance).to.be.lessThanOrEqual(
+          pairTokenBBalance - ethers.parseEther("18")
+        );
+      });
+    });
+    describe("Add and Remove Liquidity After Swaps", function () {
+      beforeEach(async function () {
+        // Add initial liquidity to the pool
+        const amountADesired = ethers.parseEther("100");
+        const amountBDesired = ethers.parseEther("200");
+        const amountAMin = ethers.parseEther("90");
+        const amountBMin = ethers.parseEther("180");
+        const latestBlock = await ethers.provider.getBlock("latest");
+
+        if (!latestBlock) {
+          throw new Error("Failed to fetch the latest block.");
+        }
+
+        const deadline = latestBlock.timestamp + 3600;
+
+        await tokenA.connect(owner).approve(router.target, amountADesired);
+        await tokenB.connect(owner).approve(router.target, amountBDesired);
+
+        await router.addLiquidity(
+          tokenA.target,
+          tokenB.target,
+          amountADesired,
+          amountBDesired,
+          amountAMin,
+          amountBMin,
+          ownerAddress,
+          deadline
+        );
+
+        // Simulate user1 swapping TokenA for TokenB
+        user1Address = await user1.getAddress();
+        const swapAmount = ethers.parseEther("10");
+        await tokenA.connect(owner).transfer(user1Address, swapAmount);
+        await tokenA.connect(user1).approve(router.target, swapAmount);
+        await router.connect(user1).swapExactTokensForTokens(
+          swapAmount,
+          ethers.parseEther("18"), // Min amount out
+          [tokenA.target, tokenB.target],
+          user1Address,
+          deadline
+        );
+
+        // Simulate user2 swapping TokenB for TokenA
+        user2Address = await user2.getAddress();
+        const reverseSwapAmount = ethers.parseEther("15");
+        await tokenB.connect(owner).transfer(user2Address, reverseSwapAmount);
+        await tokenB.connect(user2).approve(router.target, reverseSwapAmount);
+        await router.connect(user2).swapExactTokensForTokens(
+          reverseSwapAmount,
+          ethers.parseEther("6"), // Min amount out
+          [tokenB.target, tokenA.target],
+          await user2.getAddress(),
+          deadline
+        );
+      });
+
+      it("should add liquidity successfully after swaps", async function () {
+        const amountADesired = ethers.parseEther("50");
+        const amountBDesired = ethers.parseEther("100");
+        const amountAMin = ethers.parseEther("40");
+        const amountBMin = ethers.parseEther("80");
+        const latestBlock = await ethers.provider.getBlock("latest");
+
+        if (!latestBlock) {
+          throw new Error("Failed to fetch the latest block.");
+        }
+
+        const deadline = latestBlock.timestamp + 3600;
+
+        const pairAddress = await router.pairFor(tokenA.target, tokenB.target);
+        const initialPairTokenABalance = await tokenA.balanceOf(pairAddress);
+        const initialPairTokenBBalance = await tokenB.balanceOf(pairAddress);
+
+        await tokenA.connect(owner).approve(router.target, amountADesired);
+        await tokenB.connect(owner).approve(router.target, amountBDesired);
+
+        await router.addLiquidity(
+          tokenA.target,
+          tokenB.target,
+          amountADesired,
+          amountBDesired,
+          amountAMin,
+          amountBMin,
+          ownerAddress,
+          deadline
+        );
+
+        const finalPairTokenABalance = await tokenA.balanceOf(pairAddress);
+        const finalPairTokenBBalance = await tokenB.balanceOf(pairAddress);
+
+        expect(finalPairTokenABalance).to.be.greaterThan(
+          initialPairTokenABalance
+        );
+        expect(finalPairTokenBBalance).to.be.greaterThan(
+          initialPairTokenBBalance
+        );
+      });
+
+      it("should remove liquidity successfully after swaps", async function () {
+        const latestBlock = await ethers.provider.getBlock("latest");
+
+        if (!latestBlock) {
+          throw new Error("Failed to fetch the latest block.");
+        }
+
+        const deadline = latestBlock.timestamp + 3600;
+        const pairAddress = await router.pairFor(tokenA.target, tokenB.target);
+
+        const pair: IcePond = pairTokenFactory.attach(pairAddress) as IcePond;
+        const ownerLiquidityBalance = await pair.balanceOf(ownerAddress);
+
+        const initialOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
+        const initialOwnerTokenBBalance = await tokenB.balanceOf(ownerAddress);
+
+        const pairTokenABalance = await tokenA.balanceOf(pairAddress);
+        const pairTokenBBalance = await tokenB.balanceOf(pairAddress);
+
+        await pair.connect(owner).approve(router.target, ownerLiquidityBalance);
+
+        await router.removeLiquidity(
+          tokenA.target,
+          tokenB.target,
+          ownerLiquidityBalance,
+          ethers.parseEther("45"),
+          ethers.parseEther("90"),
+          ownerAddress,
+          deadline
+        );
+
+        const finalOwnerTokenABalance = await tokenA.balanceOf(ownerAddress);
+        const finalOwnerTokenBBalance = await tokenB.balanceOf(ownerAddress);
+
+        const finalPairTokenABalance = await tokenA.balanceOf(pairAddress);
+        const finalPairTokenBBalance = await tokenB.balanceOf(pairAddress);
+
+        expect(finalOwnerTokenABalance).to.be.greaterThan(
+          initialOwnerTokenABalance
+        );
+        expect(finalOwnerTokenBBalance).to.be.greaterThan(
+          initialOwnerTokenBBalance
+        );
+        expect(finalPairTokenABalance).to.be.lessThan(pairTokenABalance);
+        expect(finalPairTokenBBalance).to.be.lessThan(pairTokenBBalance);
       });
     });
   });
