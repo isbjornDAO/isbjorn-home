ec7393498c294a2b027bcc337b9a5ce8d214bfa2
Mach1Btc <allxcaps@gmail.com>
2024-12-05 18:18:25 -0600

    add quick links from manage liq and swap panels

--- Dex/client/src/_root/pages/ManageLiq.tsx
+++ Dex/client/src/_root/pages/ManageLiq.tsx
@@ -1,10 +1,14 @@
 import { LiquidityPanel } from '@/components/shared';
+import { Link } from 'react-router-dom';
 
 const ManageLiq = () => {
     return (
         <div className="flex flex-col items-center w-full h-full overflow-y-auto">
             <div className="flex flex-col w-4/5 xl:w-3/4 h-full gap-4 items-center mt-[180px] xl:mb-0">
                 <LiquidityPanel />
+                <div className='w-[640px] flex flex-row items-end justify-end'>
+                    <Link className='text-xs blue-link' to="/swap">Back to Swap</Link>
+                </div>
             </div>
         </div>
     )
--- Dex/client/src/_root/pages/Swap.tsx
+++ Dex/client/src/_root/pages/Swap.tsx
@@ -1,11 +1,15 @@
 import { SwapPanel } from '@/components/shared';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Swap = () => {
     return (
         <div className="flex flex-col items-center w-full h-full overflow-y-auto">
             <div className="flex flex-col w-4/5 xl:w-3/4 h-full gap-4 items-center mt-[180px] xl:mb-0">
                 <SwapPanel />
+                <div className='w-[640px] flex flex-row items-end justify-end'>
+                    <Link className='text-xs blue-link' to="/manageLiq">Manage Liquidity</Link>
+                </div>
             </div>
         </div>
     )
